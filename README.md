e395ed27c50fcf1bfeb56aeef5f3f3ec1a35f742
Reard Gjoni <reard.gjoni97@hotmail.co.uk>
2020-07-04 20:18:05 +0200

    vscode settings, .gitignore, README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
 My collection of dotfiles to get up and running quick in a new environment
+
+# Aren't there a ton of dotfiles repos out there?
+
+Yes, but this is mine, tailored to my needs and it's also a fun little experiment to learn some shell scripting and linux
+
+---
+
+- Run the start script to get started
--- start.sh
+++ start.sh
@@ -9,7 +9,18 @@ copyBashRc() {
     source ~/.bashrc
 }
 
+copyVsCodeSettings() {
+    SETTINGSJSON="$PWD/settings.json"
+    VSCONFDIR=~/.config/Code/User/
+    
+    echo "Copying settings.json into $VSCONFDIR..."
+    cp $SETTINGSJSON $VSCONFDIR
+}
+
 echo "Running copyBashRc"
 copyBashRc
 echo "end"
 
+echo "Running copyBashRc"
+copyVsCodeSettings
+echo "end"
